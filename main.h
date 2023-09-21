66c23c9eca5d27db32765431dcf00730ba0f8d51
evarist1 <evaristchibueze@gmail.com>
2023-09-21 16:34:54 -0700

    update

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void printString(const char *str);
+void printChar(int c);
+int handleSpecifier(const char *format, va_list args);
+int _printf(const char *format, ...);
+
+#endif
